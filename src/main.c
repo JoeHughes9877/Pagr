4d8a551e820dec45612cb55807af96b268a13643
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-06 20:55:00 +0100

    checking for file type added

--- src/main.c
+++ src/main.c
@@ -1,11 +1,23 @@
 #include <ncurses.h>
 #include <panel.h>
 #include <stdlib.h>
+#include <string.h>
 
-void usagecheck(int argc);
+bool usage_check(int argc);
+char *file_format(char *file);
 
 int main(int argc, char *argv[]) {
-  usagecheck(argc);
+
+  if (usage_check(argc) == false) {
+    printf("Usage: ./pagr <filename.filetype>\n");
+    exit(0);
+  }
+  char *file_type = file_format(argv[1]);
+
+  if (file_type == NULL) {
+    printf("Invalid file type.\n");
+    exit(0);
+  }
 
   initscr();
   printw("Pagr!");
@@ -16,9 +28,30 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-void usagecheck(int argc) {
-  if (argc != 2) {
-    printf("Usage: ./pagr <filename.filetype>\n");
-    exit(0);
+bool usage_check(int num_of_args) {
+  if (num_of_args != 2) {
+    return false;
   }
+  return true;
+}
+
+#include <stdio.h>
+#include <string.h>
+
+char *file_format(char *file) {
+
+  // NULL is needed to determin length of the arrray during runtime
+  char *accepted_file_formats[] = {"txt", "pdf", NULL};
+  int arr_len = 0;
+
+  while (accepted_file_formats[arr_len] != NULL) {
+    arr_len++;
+  }
+
+  for (int i = 0; i < arr_len; i++) {
+    if (strstr(file, accepted_file_formats[i]) != NULL) {
+      return accepted_file_formats[i];
+    }
+  }
+  return NULL;
 }
