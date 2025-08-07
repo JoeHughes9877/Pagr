de56fd786517528c4f9925e2660eebb85519dd34
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-07 01:05:04 +0100

    prints text to terminal, accepts "n" for new page and c for close

--- file_formats/txt.c
+++ file_formats/txt.c
@@ -1,29 +1,42 @@
+#include "../include/utils.h"
 #include <ncurses.h>
 #include <stdio.h>
 
-void reading_loop(char *contents);
+void reading_loop(char *contents, char *file_name);
+
+static int num = 1;
 
 void open_txt_file(char *file_name) {
   FILE *fptr;
+  num++;
 
   fptr = fopen(file_name, "r");
 
   char myString[10000];
 
-  fgets(myString, 100, fptr);
+  fgets(myString, 1000 * num, fptr);
 
-  reading_loop(myString);
+  reading_loop(myString, file_name);
 }
 
-void reading_loop(char *contents) {
-  initscr();
-  cbreak();
-  noecho();
-  keypad(stdscr, TRUE);
-  mvprintw(1, 1, "%s", contents);
-
+void reading_loop(char *contents, char *file_name) {
+  WINDOW *win = create_window();
   refresh();
-  getch();
 
-  endwin();
+  mvwprintw(win, 1, 1, "%s", contents);
+  wrefresh(win);
+
+  int ch;
+  while (1) {
+    ch = getch();
+
+    if (ch == 'n') {
+      wclear(win);
+      open_txt_file(file_name);
+    } else if (ch == 'c') {
+      delwin(win);
+      endwin();
+      break;
+    }
+  }
 }
