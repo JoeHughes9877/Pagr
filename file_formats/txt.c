257d37eb3e3362db2993e39ddb725b0c7d139221
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-07 16:03:06 +0100

    this was supposed to be easy

--- file_formats/txt.c
+++ file_formats/txt.c
@@ -1,10 +1,12 @@
 #include "../include/utils.h"
 #include <ncurses.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void reading_loop(char *contents, char *file_name);
 
-static int num = 1;
+int num = 0;
 
 void open_txt_file(char *file_name) {
   FILE *fptr;
@@ -23,15 +25,31 @@ void reading_loop(char *contents, char *file_name) {
   WINDOW *win = create_window();
   refresh();
 
-  mvwprintw(win, 1, 2, "%s", contents);
-  wrefresh(win);
+  int total_rows, total_cols;
+  int lines = 0;
+
+  getmaxyx(win, total_rows, total_cols);
+
+  char *line = malloc(total_cols + 1);
+
+  int current_offset = 0;
+
+  while (1) {
+    strcpy(&contents[current_offset], line);
+    current_offset += strlen(line);
+    contents[current_offset++] = '\n';
+    mvwprintw(win, 5, 10, "%s", line);
+
+    wrefresh(win);
+    lines++;
+  }
 
   int ch;
   while (1) {
     ch = getch();
 
     if (ch == 'n') {
-      clear();
+      wclear(win);
       open_txt_file(file_name);
     } else if (ch == 'c') {
       delwin(win);
