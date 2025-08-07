2148a729250d9cf171d8c0696ea2def1737872d6
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-07 23:53:00 +0100

    adding read file so i can save place read to, for new page

--- file_formats/txt.c
+++ file_formats/txt.c
@@ -3,12 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-void txt_reading_loop(const char *file_name) {
+FILE *read_file(char *file_name) {
   FILE *fp = fopen(file_name, "r");
   if (!fp) {
     perror("Failed to open file");
-    return;
   }
+  return fp;
+}
+
+void txt_reading_loop(const char *file_name, FILE **fp) {
 
   WINDOW *page = create_window();
 
@@ -18,7 +21,7 @@ void txt_reading_loop(const char *file_name) {
   char line[cols - 1];
   int line_num = 0;
 
-  while (line_num < rows - 2 && fgets(line, sizeof(line), fp) != NULL) {
+  while (line_num < rows - 2 && fgets(line, sizeof(line), *fp) != NULL) {
     size_t len = strlen(line);
     if (len > 0 && line[len - 1] == '\n') {
       line[len - 1] = '\0';
@@ -32,8 +35,8 @@ void txt_reading_loop(const char *file_name) {
     int ch = wgetch(page);
     if (ch == 'n') {
       wclear(page);
-      line_num = 0;
-      break;
+      wrefresh(page);
+      fclose(*fp);
     }
   }
 }
