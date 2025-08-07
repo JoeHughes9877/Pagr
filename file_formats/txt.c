e9a59fabc0569aaddbaa162fb7432be47fd667b0
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-07 22:38:47 +0100

    implemeted reading line by line

--- file_formats/txt.c
+++ file_formats/txt.c
@@ -1,60 +1,23 @@
 #include "../include/utils.h"
 #include <ncurses.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-void reading_loop(char *contents, char *file_name);
-
-int num = 0;
-
-void open_txt_file(char *file_name) {
+char *read_line(char *file_name, int win_width) {
+  int num;
   FILE *fptr;
-  num++;
 
-  fptr = fopen(file_name, "r");
-
-  char myString[10000];
-
-  fgets(myString, 1000 * num, fptr);
-
-  reading_loop(myString, file_name);
-}
-
-void reading_loop(char *contents, char *file_name) {
-  WINDOW *win = create_window();
-  refresh();
-
-  int total_rows, total_cols;
-  int lines = 0;
-
-  getmaxyx(win, total_rows, total_cols);
-
-  char *line = malloc(total_cols + 1);
-
-  int current_offset = 0;
-
-  while (1) {
-    strcpy(&contents[current_offset], line);
-    current_offset += strlen(line);
-    contents[current_offset++] = '\n';
-    mvwprintw(win, 5, 10, "%s", line);
-
-    wrefresh(win);
-    lines++;
+  char *buffer = malloc(win_width + 1);
+  if ((fptr = fopen(file_name, "r")) == NULL) {
+    printf("Error! opening file");
+    exit(1);
   }
 
-  int ch;
-  while (1) {
-    ch = getch();
+  size_t line = fwrite(buffer, sizeof(char), win_width, fptr);
+  buffer[line] = '\0';
 
-    if (ch == 'n') {
-      wclear(win);
-      open_txt_file(file_name);
-    } else if (ch == 'c') {
-      delwin(win);
-      endwin();
-      break;
-    }
-  }
+  fclose(fptr);
+
+  return buffer;
 }
+
+void txt_reading_loop(char *file_name) {}
