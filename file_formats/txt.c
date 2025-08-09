fb2404c27aa9807217d3075a0278d07dd21bf884
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-09 01:03:54 +0100

    turn page

--- file_formats/txt.c
+++ file_formats/txt.c
@@ -9,7 +9,6 @@
 
 FILE *read_line(char *file_name) {
   setlocale(LC_ALL, "");
-  initscr();
 
   FILE *fptr;
   if ((fptr = fopen(file_name, "r")) == NULL) {
@@ -22,38 +21,39 @@ FILE *read_line(char *file_name) {
 }
 
 void txt_reading_loop(const char *file_name) {
-  WINDOW *page = create_window();
-
-  int rows, cols;
-  getmaxyx(page, rows, cols);
-
   FILE *fp = read_line((char *)file_name);
 
-  wchar_t line[cols - 1];
-  int line_num = 0;
-
-  while (line_num < rows - 2 && fgetws(line, cols, fp) != NULL) {
-    size_t len = wcslen(line);
-    if (len > 0 && line[len - 1] == '\n') {
-      line[len - 1] = '\0';
-    }
-
-    mvwaddnwstr(page, line_num + 1, 1, line, cols - 2);
-    line_num++;
-  }
-
-  wrefresh(page);
   while (1) {
-    int ch = wgetch(page);
-    if (ch == 'n') {
-    } else if (ch == 'q') {
-      delwin(page);
-      fclose(fp);
-      endwin();
-      exit(0);
+    WINDOW *page = create_window();
+
+    int rows, cols;
+    getmaxyx(page, rows, cols);
+
+    wchar_t line[cols - 1];
+    int line_num = 0;
+
+    while (line_num < rows - 2 && fgetws(line, cols, fp) != NULL) {
+      size_t len = wcslen(line);
+      if (len > 0 && line[len - 1] == '\n') {
+        line[len - 1] = '\0';
+      }
+
+      mvwaddnwstr(page, line_num + 1, 1, line, cols - 2);
+      line_num++;
+    }
+
+    wrefresh(page);
+    while (1) {
+      int ch = wgetch(page);
+      if (ch == 'n') {
+        delwin(page);
+        break;
+      } else if (ch == 'q') {
+        delwin(page);
+        fclose(fp);
+        endwin();
+        exit(0);
+      }
     }
   }
-
-  fclose(fp);
-  wrefresh(page);
 }
