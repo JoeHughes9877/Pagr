a85b46d233e4aeebedb6f62ccd230974b3dc276f
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-10 22:56:55 +0100

    end of book added

--- file_formats/plain_txt.c
+++ file_formats/plain_txt.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
 #define _XOPEN_SOURCE_EXTENDED 1
 
 #include "../include/utils.h"
 #include <ncurses.h>
-#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <wchar.h>
 
-// arbatary number, place holder for logic before i use dynamic mem allocation.
+// arbatary number, i think no books will pass.
 #define MAX_PAGES 10000
 long page_offsets[MAX_PAGES];
 int current_page = 0;
 int total_pages = 0;
+int eof = 0;
 
 void plain_txt_reading_loop(const char *file_name) {
   FILE *fp = open_plain_txt_file((char *)file_name);
@@ -25,36 +25,39 @@ void plain_txt_reading_loop(const char *file_name) {
 
     wchar_t line[cols - 1];
     int line_num = 0;
+    int page_eof = 0;
 
     while (line_num < rows - 2 && fgetws(line, cols, fp) != NULL) {
-
       wchar_t *nl = wcschr(line, L'\n');
-      if (nl) {
+      if (nl)
         *nl = L'\0';
-      }
-
       mvwaddnwstr(page, line_num + 1, 1, line, cols - 2);
       line_num++;
     }
+
     mvwprintw(page, line_num + 1, 1, "Page Number: %d", current_page);
 
     wrefresh(page);
+
+    if (feof(fp)) {
+      page_eof = 1;
+      mvwprintw(page, line_num + 2, 1, "End of book.");
+    }
+    wrefresh(page);
+
     while (1) {
       int ch = wgetch(page);
-      if (ch == 'n') { // next page
+      if (ch == 'n' && !page_eof) {
         delwin(page);
         current_page++;
         break;
-      } else if (ch == 'b' && current_page > 0) { // prev page
+      } else if (ch == 'b' && current_page > 0) {
         delwin(page);
         current_page--;
         fseek(fp, page_offsets[current_page], SEEK_SET);
         break;
-      } else if (ch == 'q') { // close
-        delwin(page);
-        fclose(fp);
-        endwin();
-        exit(0);
+      } else if (ch == 'q') {
+        close_application(page, fp);
       }
     }
   }
