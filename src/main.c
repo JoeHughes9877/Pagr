699a24c8f3be113a7596bf75c69c1601557ff0cc
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-11 18:51:16 +0100

    clean up and reformatting

--- src/main.c
+++ src/main.c
@@ -1,53 +1,59 @@
 #include "../include/plain_txt.h"
-#include "../include/utils.h"
 #include <ncurses.h>
 #include <panel.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 
-bool usage_check(int argc);
-int file_format(char *file);
+void usage_check(int argc);
+bool is_valid_extension(char *file);
+void open_file(char *file_to_open);
+;
 
-const int arr_len = 16;
 const char *acceptedFormats[] = {"txt",  "md",  "csv", "log",  "json", "xml",
                                  "yaml", "yml", "ini", "conf", "cfg",  "rtf",
                                  "tex",  "asc", "bat", "sh",   "htm"};
+static const size_t arr_len =
+    sizeof(acceptedFormats) / sizeof(acceptedFormats[0]);
 
-int main(int argc, char *argv[]) {
-
-  if (usage_check(argc) == false) {
+void usage_check(int num_of_args) {
+  if (num_of_args == 2) {
+    return;
+  } else {
     printf("Usage: ./pagr <filename.filetype>\n");
     exit(0);
   }
+}
+
+bool is_valid_extension(char *file) {
+  for (size_t i = 0; i < arr_len; i++) {
+    if (strstr(file, acceptedFormats[i]) != NULL) {
+      return true;
+    }
+  }
+  printf("Invalid file type.\n");
+  exit(0);
+}
+
+void read_book(char *file_to_open) {
+  for (size_t i = 0; i < arr_len; i++) {
+    if (strstr(file_to_open, acceptedFormats[i])) {
+      plain_txt_reading_loop(file_to_open);
+      break;
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+
+  usage_check(argc);
 
   char *file_to_open;
-  if (file_format(argv[1]) == 1) {
+  if (is_valid_extension(argv[1])) {
     file_to_open = argv[1];
-  } else {
-    printf("Invalid file type.\n");
-    exit(0);
   }
 
-  for (int i = 0; i < arr_len; i++) {
-    if (strstr(file_to_open, acceptedFormats[i]) == 0) {
-      plain_txt_reading_loop(file_to_open);
-    }
-  }
-  return 0;
-}
+  read_book(file_to_open);
 
-bool usage_check(int num_of_args) {
-  if (num_of_args != 2) {
-    return false;
-  }
-  return true;
-}
-
-int file_format(char *file) {
-  for (int i = 0; i < arr_len; i++) {
-    if (strstr(file, acceptedFormats[i]) != NULL) {
-      return 1;
-    }
-  }
   return 0;
 }
