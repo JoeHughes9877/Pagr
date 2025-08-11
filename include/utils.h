f039ee7e68589615c689d39310dcd6982315c92f
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-11 15:13:22 +0100

    #

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,9 @@
 #include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 WINDOW *create_window();
-char *controller(WINDOW *win);
+
 FILE *open_plain_txt_file(char *file_name);
+
 void close_application(WINDOW *page, FILE *fp);
