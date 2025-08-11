9b5f286cb53d269fa9bfc3c4a4d9f6427b806362
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-08-11 20:56:25 +0100

    added dif themes

--- src/themes.c
+++ src/themes.c
@@ -1,37 +1,27 @@
 #include <ncurses.h>
 #include <string.h>
 
-void init_theme_classic() {
-  start_color();
-  init_pair(1, COLOR_RED, COLOR_BLACK);
-  init_pair(2, COLOR_WHITE, COLOR_BLACK);
-  init_pair(3, COLOR_YELLOW, COLOR_BLACK);
-  init_pair(4, COLOR_BLACK, COLOR_WHITE);
-}
+void init_theme_classic() { init_pair(1, COLOR_WHITE, COLOR_BLACK); }
 
-void init_theme_ocean() {
-  start_color();
-  init_pair(1, COLOR_CYAN, COLOR_BLACK);
-  init_pair(2, COLOR_BLUE, COLOR_BLACK);
-  init_pair(3, COLOR_WHITE, COLOR_BLUE);
-  init_pair(4, COLOR_YELLOW, COLOR_BLACK);
-}
+void init_theme_dracula() { init_pair(1, COLOR_MAGENTA, COLOR_BLACK); }
 
-void init_theme_solarized() {
-  start_color();
-  init_pair(1, COLOR_YELLOW, COLOR_BLUE);
-  init_pair(2, COLOR_WHITE, COLOR_BLACK);
-  init_pair(3, COLOR_CYAN, COLOR_BLACK);
-  init_pair(4, COLOR_BLACK, COLOR_YELLOW);
-}
+void init_theme_gruvbox() { init_pair(1, COLOR_RED, COLOR_BLACK); }
+
+void init_theme_nord() { init_pair(1, COLOR_BLUE, COLOR_BLACK); }
+
+void init_theme_monokai() { init_pair(1, COLOR_YELLOW, COLOR_BLACK); }
 
 void choose_theme(const char *theme_name) {
-  if (strcmp(theme_name, "classic") == 0) {
-    init_theme_classic();
-  } else if (strcmp(theme_name, "ocean") == 0) {
-    init_theme_ocean();
-  } else if (strcmp(theme_name, "solarized") == 0) {
-    init_theme_solarized();
+  if (theme_name == NULL) {
+    init_theme_dracula(); // default theme
+  } else if (strcmp(theme_name, "dracula") == 0) {
+    init_theme_dracula();
+  } else if (strcmp(theme_name, "gruvbox") == 0) {
+    init_theme_gruvbox();
+  } else if (strcmp(theme_name, "nord") == 0) {
+    init_theme_nord();
+  } else if (strcmp(theme_name, "monokai") == 0) {
+    init_theme_monokai();
   } else {
     init_theme_classic();
   }
